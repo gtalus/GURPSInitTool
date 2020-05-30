8618c98757e9547c0e66339a969205d3e0df063b
gtalus <gtalus@users.noreply.github.com>
2020-05-30 07:54:11 -0700

    update year

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GURPSInitTool'
-copyright = u'2015, talus'
+copyright = u'2020, talus'
 author = u'talus'
 
 # The version info for the project you're documenting, acts as replacement for
